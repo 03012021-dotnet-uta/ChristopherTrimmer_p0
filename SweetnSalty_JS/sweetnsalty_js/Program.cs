42b964f64090327787038e1dcd56f7c4b803e641
Christopher Trimmer <chrisschoolacct@yahoo.com>
2021-03-15 16:59:34 -0400

    adding sweetnsalty_js coding project

---
+++ SweetnSalty_JS/sweetnsalty_js/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sweetnsalty_js
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
